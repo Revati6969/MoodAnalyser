016d56d2e5804dde5234648d30e6b61777554b0c
Revati Tekale <revitekale1910@gmail.com>
2020-03-06 18:03:26 +0530

    ADDED:Mood message in constructor

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -2,8 +2,23 @@ public class MoodAnalyzer {
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyser");
     }
+    private String message;
 
-    public String analyseMood (String message)
+    public MoodAnalyzer(){
+    }
+
+    public MoodAnalyzer(String message)
+    {
+        this.message=message;
+    }
+
+    public String analyseMood(String message)
+    {
+        this.message=message;
+        return analyseMood();
+    }
+
+    public String analyseMood ()
     {
         if (message.contains("Sad"))
             return "SAD";
--- src/test/java/TestMoodAnalyser.java
+++ src/test/java/TestMoodAnalyser.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMoodAnalyser {
+
     MoodAnalyzer MoodAnalyser=new MoodAnalyzer();
 
     @Test
@@ -9,9 +10,11 @@ public class TestMoodAnalyser {
         String result=MoodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("SAD",result);
     }
+
     @Test
     public void givenMessage_whenResponse_thenHappy() {
         String result1=MoodAnalyser.analyseMood("I am in Any Mood");
         Assert.assertEquals("HAPPY",result1);
     }
+
 }
