ffea27502368ed331ebfa32dd9297112f1a0641e
Revati Tekale <revitekale1910@gmail.com>
2020-03-11 10:21:30 +0530

    ADDED:Handled null pointer exception

--- src/test/java/TestMoodAnalyser.java
+++ src/test/java/TestMoodAnalyser.java
@@ -6,15 +6,23 @@ public class TestMoodAnalyser {
     MoodAnalyzer MoodAnalyser=new MoodAnalyzer();
 
     @Test
-    public void givenMessage_whenResponse_thenSad() {
+    public void givenMessage_WhenResponse_ThenSad() {
         String result=MoodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("SAD",result);
     }
 
     @Test
-    public void givenMessage_whenResponse_thenHappy() {
+    public void givenMessage_WhenResponse_ThenHappy() {
         String result1=MoodAnalyser.analyseMood("I am in Any Mood");
         Assert.assertEquals("HAPPY",result1);
     }
+
+    @Test
+    public void givenNull_WhenResponse_ThenHappy() {
+        String result1=MoodAnalyser.analyseMood(null);
+        Assert.assertEquals("HAPPY",result1);
+    }
+
+
 
 }
